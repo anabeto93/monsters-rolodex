5188e14530f307457879765d28d066b4b21046c9
Richard Anabeto Opoku <anabeto93@gmail.com>
2020-06-13 10:27:19 +0000

    Updates

---
+++ precache-manifest.6668faf757bf02f97eae221a58a7e829.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7813980e88e36b7aa0d46ad8197663ed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "800cf5f2e5de3f2ee26a",
+    "url": "/monsters-rolodex/static/css/main.1e881b03.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800cf5f2e5de3f2ee26a",
+    "url": "/monsters-rolodex/static/js/main.015df00b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
